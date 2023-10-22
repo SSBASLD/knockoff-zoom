20d478f050411685cd6bebd9a6e95023bc584fc0
SSBASLD <77936828+SSBASLD@users.noreply.github.com>
2023-10-22 11:03:22 -0500

    Added another chat page to test websockets

---
+++ chat-page/other-chat-page/script.js
@@ -0,0 +1,18 @@
+let client = new WebSocket("wss://node-websocket-server-a4uv.onrender.com/ws/", ['echo-protocol', 'recieving']);
+client.onopen = () => {
+    console.log("Connection Established");
+}
+
+let textArea = document.getElementById("chatTextArea");
+let sendButton = document.getElementById("sendButton");
+sendButton.onclick = () => {
+    let value = textArea.value;
+    let jsonText = `{"message": ${value}}`;
+
+    client.send(jsonText);
+}
+
+let returnTextArea = document.getElementById("returnTextArea");
+client.onmessage = (message) => {
+    returnTextArea.innerHTML = message.data;
+};
