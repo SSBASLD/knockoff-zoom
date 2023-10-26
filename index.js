800fa87c1b50556c6f0a7ff472ae808fc8e59ecb
PotmanNob <Potmannob@outlook.com>
2023-10-26 15:34:16 -0500

    Made functions for different aspect of client

--- index.js
+++ index.js
@@ -1,7 +1,49 @@
+class Message{
+    constructor(head, content) {
+        this.head = head;
+        this.content = content;
+    }
+}   
+
+
+const _iceServers = {'iceServers': [{'urls': 'stun:stun3.l.google.com:19302'}]} // sets up which stun server to use.
+var connections = [] 
+
 let client = new WebSocket('ws://localhost:8080', ['echo-protocol']);
 client.onopen = () => {
-    console.log("hi");
+
+    client.onmessage = function (event) {
+        try {
+            var message = JSON.parse(event.utf8Data);
+            switch (message.head) {
+                case "value":
+                    
+                    break;
+            
+                default:
+                    break;
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
 }
-client.onmessage = function (event) {
-    console.log(event.data);
+
+
+async function makeCall() {
+    const pc = new RTCPeerConnection(_iceServersiceServers); // Creates a new RTCPeerConnection instance with ICE candidates recieved from stun server
+    const offer = await pc.createOffer(); // Await bc it returns a promise
+    await pc.setLocalDescription(offer); // Await bc it returns a promise
+    client.send(new Message("callRequest", offer))
+}
+
+async function acceptCall(offer) {
+    const pc = new RTCPeerConnection(_iceServers)
+    await pc.setRemoteDescription(new RTCSessionDescription(offer));
+    const answer = await pc.createAnswer();
+    await pc.setLocalDescription(answer);
+    client.send(new Message("acceptRequest", answer));
+}
+async function handleAccept(acceptOffer) {
+    await pc.setRemoteDescription(new RTCSessionDescription(acceptOffer));
 }
