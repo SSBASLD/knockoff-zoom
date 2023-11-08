6659bd1b94714e487537437c7cd1dff92caea91f
SSBASLD <77936828+SSBASLD@users.noreply.github.com>
2023-11-07 19:11:46 -0600

    Made the chat window look a lot better

--- room-connect/chat-page/script.js
+++ room-connect/chat-page/script.js
@@ -1,57 +1,79 @@
 let chatSquare = document.getElementById("Chat Square");
 chatSquare.style.display = "none";
 
-let leftTextArea = document.getElementById("Left Return Text Area");
-let rightTextArea = document.getElementById("Right Return Text Area");
-
 let textArea = document.getElementById("chatInput");
 let arrowButton = document.getElementById("Arrow Button");
 
-let counter = 0;
+let chatWindow = document.getElementById("Chat Window");
+
+let diff = 9;
+let topPercentage = 2;
+
+let oneLineHeight = document.getElementById("oneLineHeight").scrollHeight;
+let twoLineHeight = document.getElementById("twoLineHeight").scrollHeight;
+let lineDiff = twoLineHeight - oneLineHeight;
+
+var person = 2;
 arrowButton.onclick = () => {
-    if (person != null) {
-        if (jsonData.person == 1) {
-            leftTextArea.innerHTML += "Person 1: ";
-            lineBreakInsertor(jsonData.message, "left");
-        } else {
-            rightTextArea.innerHTML += "Person 2: ";
-            lineBreakInsertor(jsonData.message, "right");
-        }
-    }
 
     let value = textArea.value;
     let jsonText = `{"message": "${value}", "roomKey": "${roomKey}"}`;
 
     client.send(jsonText);
-}
 
-function lineBreakInsertor(text, side) {
-    console.log(person);
-    
-    for (let i = 0; i < text.length; i += 24) {
-
-        let slicedText = text.slice(i, i + 25);
-
-        let oneLineBreak = document.createElement("br");
-        if (side == "left") {
-            leftTextArea.innerHTML += slicedText;
-            leftTextArea.appendChild(oneLineBreak);
-            rightTextArea.appendChild(oneLineBreak);
-        } else if (side == "right") {
-            rightTextArea.innerHTML += slicedText;
-            rightTextArea.appendChild(oneLineBreak);
-            leftTextArea.appendChild(oneLineBreak);
+    let oneLineBreak = document.createElement("br");
+    let twoLineBreak = document.createElement("br");
+    if (person != null) {
+        if (person == 1) {
+            createTextArea(value, "left");
+        } else {
+            createTextArea(value, "right");
         }
     }
-
-    if (side == "left") {
-        rightTextArea.appendChild(oneLineBreak);
-    } else if (side == "right") {
-        rightTextArea.appendChild(oneLineBreak);
-    }
 }
 
-let person;
+function createTextArea(value, side) {
+    let numOfLines;
+    if (side == "left") {
+        let bluePerson = document.createElement("img");
+        bluePerson.className = "person";
+        bluePerson.src = "../../images/BluePerson.png"
+        bluePerson.style.left = "7%";
+        bluePerson.style.top = `${topPercentage}%`;
+        chatWindow.appendChild(bluePerson);
+    
+        let leftTextArea = document.createElement("div");
+        leftTextArea.className = "leftTextArea";
+    
+        leftTextArea.innerHTML += value;
+        leftTextArea.style.top = `${topPercentage}%`;
+    
+        chatWindow.appendChild(leftTextArea);
+    
+        let heightLineDivision = Math.floor((leftTextArea.scrollHeight - oneLineHeight) / lineDiff);
+        numOfLines = heightLineDivision + 1;
+    } else if (side == "right") {
+        let greenPerson = document.createElement("img");
+        greenPerson.className = "person";
+        greenPerson.src = "../../images/GreenPerson.png"
+        greenPerson.style.left = "92%";
+        greenPerson.style.top = `${topPercentage}%`;
+        chatWindow.appendChild(greenPerson);
+    
+        let rightTextArea = document.createElement("div");
+        rightTextArea.className = "rightTextArea";
+    
+        rightTextArea.innerHTML += value;
+        rightTextArea.style.top = `${topPercentage}%`;
+    
+        chatWindow.appendChild(rightTextArea);
+    
+        let heightLineDivision = Math.floor((rightTextArea.scrollHeight - oneLineHeight) / lineDiff);
+        numOfLines = heightLineDivision + 1;
+    }
+    
+    topPercentage += diff + 3*((numOfLines - 2) < 0 ? 0 : numOfLines - 2);
+}
 
 let roomKey;
 let client;
@@ -90,8 +112,7 @@ async function setUpSocket() {
                 lineBreakInsertor(jsonData.message, "right");
             }
         } else if (jsonData.type == "Info") {
-            let person = jsonData.person;
-            console.log(person);
+            person = jsonData.person;
         }
     };
 }
