80c914f669a354beb1a821346a7ee3814fe406d7
SSBASLD <77936828+SSBASLD@users.noreply.github.com>
2023-11-09 15:37:56 -0600

    bug fix

--- node-websocket-server/server.js
+++ node-websocket-server/server.js
@@ -74,9 +74,8 @@ wsServer.on('request', function (request) {
 
     //The heartbeat. This pings each socket that is connected to the server. They should respond back, and so the server knows its alive and will keep it alive
     const interval = setInterval(() => {
-        console.log("pinged");
-        
         connections.forEach((value, connection, map) => {
+            console.log("pinged");
             if (connection.isAlive === false) {
                 console.log("connection killed");
                 connections.delete(connection);
